49b7189e4d437a87c3a2f8bf88d7dff1dcba849b
nsalimova <nsalimova@oggettoweb.com>
2023-11-26 11:08:13 +0300

    Loaders for different types files

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,28 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.png$/,
-                use: [{ loader: 'file-loader' }]
+                test: /\.(png|jpg|jpeg|gif|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader' ,
+                        options: {
+                            outputPath: 'images',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader' ,
+                        options: {
+                            outputPath: 'fonts',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }
+                ]
             }
         ]
     }
