58ed99e82190d4ab7218341487c8b9406622c83f
nsalimova <nsalimova@oggettoweb.com>
2023-11-27 11:50:18 +0300

    Composition of loaders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import css from './main.css';
 
 const App = ()=> <p>Hello World</p>;
 
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,13 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(css)$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
             }
         ]
     }
