567a48907b4a12bc64801f593a935cd6a60024bb
nsalimova <nsalimova@oggettoweb.com>
2023-11-27 11:18:40 +0300

    React and Babel Loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ module.exports = {
     module: {
         rules: [
             {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            {
                 test: /\.(png|jpg|jpeg|gif|ico)$/,
                 use: [
                     {
